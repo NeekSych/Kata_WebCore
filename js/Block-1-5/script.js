455987b698c46e16571b41456db3dd199ef25e8d
Honor <nik26sych@gmail.com>
2023-07-10 15:58:14 +0300

    1-6 added

--- js/Block-1-5/script.js
+++ js/Block-1-5/script.js
@@ -23,4 +23,4 @@
         document.write('<script class="script" src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>');
         viewButton.style.display = 'none';
     }
-    
+    console.log(3);
